ab178b1fdcab3ac53ccd763e7211fc9e520ca557
Dmitri <dmitri.tsjornoi@gmail.com>
2019-11-20 18:00:56 +0200

    Update index.js

--- node/index.js
+++ node/index.js
@@ -1,1 +1,11 @@
-console.log('Hello world!');
+function car(carName) {
+    this.carName = carName || 'Unknown name';
+}
+
+car.prototype.logName = function(){
+    console.log('Car name is', this.carName);
+}
+
+var bmw = new car('BMW');
+bmw.logName();
+
